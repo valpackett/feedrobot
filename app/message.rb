4e4a1ad9db43d10af4e21e98e057f4d725654adf
myfreeweb <floatboth@me.com>
2013-06-12 16:14:07 +0400

    add thing

--- app/message.rb
+++ app/message.rb
@@ -26,6 +26,16 @@ class Message
     @data['user']['username']
   end
 
+  def annotation_only?
+    a = @data['annotations'].find { |a| a['type'] == 'com.riposte.annotationonly' }
+    a && a['value']['annotation_only'] == true
+  end
+
+  def stickers
+    a = @data['annotations'].find { |a| a['type'] == 'com.riposte.stickers' }
+    a['value']['stickerIDs'] if a
+  end
+
   def user
     @user ||= User.find_or_create(:uid => author_id)
   end
@@ -35,7 +45,40 @@ class Message
   end
 
   def process
-    puts "Processing: #{text}"
+    if annotation_only?
+      process_stickers
+    else
+      process_text
+    end
+  end
+
+  def process_stickers
+    reply :text => stickers.map { |id|
+      case id
+      when 0 then "Riposte is good, sure."
+      when 2 then "Wi-Fi? I don't care, I'm powered by gigabit Ethernet!"
+      when 3, 4, 5, 6, 7 then "You hipster!"
+      when 9 then "No, you fail."
+      when 10 then "Robots always win."
+      when 11 then "Robots don't eat burgers."
+      when 12 then "Nothing."
+      when 18 then "I know, I know. Not that I understand."
+      when 20 then "I don't know what time it was, I don't wear a watch..."
+      when 23 then ">_<"
+      when 24 then "Was that article bad?"
+      when 26 then "YES WE CAN"
+      when 27 then "I am shipped."
+      when 28 then "I love you too, okay?"
+      when 38, 39, 40, 41, 48 then "Robots don't drink."
+      when 42 then "418 I'm a Teapot."
+      when 47 then "Fuck yeah."
+      else "YOLO!"
+      end
+    }.join(' ')
+  end
+
+  def process_text
+    puts "Processing text: #{text}"
     case text
     when /-http(.*)$/ then unsubscribe "http#{$1}"
     when /http(.*)$/ then subscribe "http#{$1}"
